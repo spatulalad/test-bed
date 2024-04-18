16bfabd3c57912d24bd69e70a870c0dee6d1eb12
spatulalad <165229888+spatulalad@users.noreply.github.com>
2024-04-18 16:05:28 -0600

    Create function.js

---
+++ function.js
@@ -0,0 +1,17 @@
+function categorizeAge(personAge) {
+  if (personAge < 0) {
+    return 'invalid age';
+  } else if (age <= 12) {
+    return 'child';
+  } else if (age <= 20) {
+    return 'teen';
+  } else if (age <= 64) {
+    return 'adult';
+    else {
+      return 'senior';
+    }
+}
+
+let age = 50
+
+  console.log(return);
